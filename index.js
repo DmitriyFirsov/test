7d07d5cdcd9d099167484856d049e5f762015057
Dmitriy Firsov <dmitriy.firsov@flatstack.com>
2021-10-07 16:19:40 +0300

    fix

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
-import WebSocket from 'ws';
+const WebSocket = require('ws');
 
 const ws = new WebSocket('ws://localhost');
 
 ws.addEventListener('error', console.log);
 ws.addEventListener('message', console.log);
+ws.addEventListener('open',()=>{
+    ws.send('some data')
+})
